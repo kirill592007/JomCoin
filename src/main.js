b038dc11f122a4c4d9f1854e842e8a34ea9fcff7
unknown <kirill592007@gmail.com>
2024-09-29 22:51:36 +0600

    fix bugs

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const {app, BrowserWindow} = require('electron');
+const {autoUpdater} = require('electron-updater')
 const path = require('path');
 
 const createWindow = () => {
@@ -7,13 +8,25 @@ const createWindow = () => {
     height: 839,
     icon: path.join(__dirname, 'icon.ico'),
     webPreferences: {
-      webSecurity: false
+      webSecurity: false,
+      nodeIntegration: true,
+      contextIsolation: false
     }
   });
   win.setMenuBarVisibility(false);
-  win.setTitle('Dot-coin');
+  win.setTitle('JomCoin');
   win.loadFile('src/index.html');
+  autoUpdater.checkForUpdatesAndNotify()
 }
+
+autoUpdater.on('update-available', () => {
+  console.log('Обновление доступно');
+})
+
+autoUpdater.on('update-downloaded', () => {
+  console.log('Обновление загружено, готово к установке');
+  autoUpdater.quitAndInstall()
+})
 
 app.whenReady().then(() => createWindow());
 app.on('window-all-closed', () => app.quit());
